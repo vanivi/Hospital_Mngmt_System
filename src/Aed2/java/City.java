217087733b65d92b111bb24ddbd1ba59228f56ca
vidhipatel1 <patel.vidhi3@northeastern.edu>
2022-10-30 18:14:27 -0400

    city coded

---
+++ src/Aed2/java/City.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aed2.java;
+
+/**
+ *
+ * @author Vidhi
+ */
+public class City {
+    private String name;
+    City(String name){
+        this.name=name;
+    }
+    public String getCityName(){
+        return name;
+    }
+    public void setCityName(String name){
+        this.name=name;
+    }
+}
